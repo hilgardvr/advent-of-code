25249f229651c2e9fe2373f77e3356f1c7874d0d
hilgard <hilgardvr@gmail.com>
2023-12-02 21:00:08 +0200

    2023/2.1 wip

---
+++ 2023/day2/src/Lib.hs
@@ -0,0 +1,104 @@
+module Lib
+    ( someFunc
+    , runapp
+    ) where
+
+import Debug.Trace (trace)
+import Data.List
+
+data Game = Game {
+    gid :: Integer
+    , samples :: [Sample]
+} deriving (Show)
+
+data Sample = Sample {
+    red :: Integer
+    , green :: Integer
+    , blue :: Integer
+} deriving (Show)
+
+emptySample :: Sample
+emptySample = Sample { red = 0, green = 0, blue = 0 }
+
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
+
+parse :: Char -> String -> (String, String)
+parse c s =  
+    case elemIndex c s of
+        Nothing -> error "No : found"
+        Just i -> splitAt i s
+
+splitAtAndRemove :: Char -> String -> [String]
+splitAtAndRemove c [] = []
+splitAtAndRemove c xs = 
+    case elemIndex c xs of 
+        Nothing -> [xs]
+        Just i -> take i xs : splitAtAndRemove c (drop (i+1) xs)
+
+trim :: String -> String
+trim = f . f
+    where f = reverse . dropWhile (== ' ')
+
+readSample :: String -> Sample
+readSample s =
+    let 
+        trm :: [String]
+        trm = splitAtAndRemove ',' (trim s)
+        prs :: [[String]]
+        prs = map ((splitAtAndRemove ' ') . trim) trm
+
+        helper :: [String] -> Sample -> Sample
+        helper xs s =
+            case head . tail $ xs of
+                "red" -> s { red = (read (head xs) :: Integer) }
+                "green" -> s { green = (read (head xs) :: Integer) }
+                "blue" -> s { blue = (read (head  xs) :: Integer) }
+                _ -> error $ "No color: " ++ (head . tail) xs
+    in
+        foldr (\e a -> helper e a) emptySample prs
+
+
+parseGame :: [String] -> Game
+parseGame s = 
+    let
+        gid = read $ drop 5 (head s) :: Integer
+        samples = head . tail $ s
+        parsedSamples :: [String]
+        parsedSamples = splitAtAndRemove ';' samples
+        dataSamples = map readSample parsedSamples
+    in
+        Game {
+            gid = gid,
+            samples = dataSamples
+        }
+
+isValidGame :: Sample -> Game -> Bool
+isValidGame s g =
+    let 
+        spls = samples g
+
+        isValidSample :: Sample -> Bool
+        isValidSample s' = red s' < red s && green s' < green s && blue s' < blue s
+    in 
+        all id (map isValidSample spls)
+    
+
+validSample :: Sample
+validSample = Sample {
+    red = 12
+    , green = 13
+    , blue = 14
+}
+
+runapp :: String -> Integer
+runapp s = 
+    let
+        ls :: [String]
+        ls = lines s
+        games = map (splitAtAndRemove ':') ls
+        readGames = map parseGame games
+        validGames = filter (isValidGame validSample) readGames
+    in
+        trace (show validGames) (sum (map gid validGames))
