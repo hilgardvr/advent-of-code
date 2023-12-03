5cf3125e141077f424595ddd6e88f7fa0be500c9
hilgard <hilgardvr@gmail.com>
2023-12-03 13:12:34 +0200

    2023/3.1

--- 2023/day3/src/Lib.hs
+++ 2023/day3/src/Lib.hs
@@ -13,6 +13,9 @@ data Coord = Coord {
     , y :: Int
 } deriving (Show)
 
+instance Eq Coord where
+    a == b = (x a == x b) && (y a == y b)
+
 numIndexLine :: Int -> Int -> String -> [(Coord, String)]
 numIndexLine x y [] = []
 numIndexLine x y xs = 
@@ -30,20 +33,65 @@ numIndexLine x y xs =
             l = length n
         in
             numIndexLine (x + l) y (drop l xs) 
+
+symbolIndexLine :: Int -> Int -> String -> [(Coord, Char)]
+symbolIndexLine x y [] = []
+symbolIndexLine x y xs = 
+    if (not . isNumber $ head xs) && head xs /= '.'
+    then (Coord {x = x, y = y}, head xs) : symbolIndexLine (x + 1) y (drop 1 xs) 
+    else symbolIndexLine (x + 1) y (drop 1 xs) 
     
 numIndexParser :: Int -> [String] -> [(Coord, String)]
 numIndexParser _ [] = []
-numIndexParser y (x:xs) = 
-    numIndexLine 0 y x ++ numIndexParser (y+1) xs
-    
+numIndexParser y' (x':xs) = numIndexLine 0 y' x' ++ numIndexParser (y'+1) xs
+
+symbolIndexParser :: Int -> [String] -> [(Coord, Char)]
+symbolIndexParser _ [] = []
+symbolIndexParser y' (x':xs) = symbolIndexLine 0 y' x' ++ symbolIndexParser (y'+1) xs
+
+adjacents :: Coord -> (Coord, String) -> [Coord]
+adjacents m (c, n)  =
+    let
+        minx = max (0) (x c - 1)
+        maxx = min (x c + length n) (x m)
+        miny = max 0 (y c - 1)
+        maxy = min (y c + 1) (y m)
+        xs = [minx..maxx]
+        ys = [miny..maxy]
+    in
+        [Coord {x = x', y = y'} | x' <- xs, y' <- ys ]
+
+numsWithAdjacents :: (Coord) -> [(Coord, String)] -> [(Coord, Char)] -> [(Coord, String)]
+numsWithAdjacents _ [] _ = []
+numsWithAdjacents max (h:t) cs = 
+    let 
+        adjs = adjacents max h
+        cCoords = map fst cs
+
+        matches :: [Coord] -> Bool
+        matches [] = False
+        matches (h:t) = 
+            if any (== h) adjs
+            then True
+            else matches t
+    in
+        if matches cCoords
+        then h : numsWithAdjacents max t cs
+        else numsWithAdjacents max t cs
 
 
-partNumbers :: String -> Integer
+partNumbers :: String -> Int
 partNumbers s = 
     let
         ls = lines s
+        numXs = (length $ head ls) - 1
+        numYs = (length ls) - 1
+        max = Coord {x = numXs, y = numYs}
         nis = numIndexParser 0 ls
+        sis = symbolIndexParser 0 ls
+        res = numsWithAdjacents max nis sis
+        total = foldr (\e a -> (read (snd e) :: Int) + a) 0 res
     in
-        trace (show nis) 0
+        trace (show res) total
 
 
