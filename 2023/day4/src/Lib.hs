407f275635d1a809c3b4dd74397059aa57be6179
Hilgard van Rooyen <hilgard@onespark.co.za>
2023-12-04 22:07:47 +0200

    2023/4.2

--- 2023/day4/src/Lib.hs
+++ 2023/day4/src/Lib.hs
@@ -1,6 +1,7 @@
 module Lib
     ( someFunc
     , runapp
+    , cardwinapp
     ) where
 import Data.List (elemIndex)
 import Debug.Trace (trace)
@@ -8,6 +9,11 @@ import Debug.Trace (trace)
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
 
+data Game = Game {
+    gid :: Int
+    , winNumbers :: [Int]
+    , myNumbers :: [Int]
+} deriving (Show, Read)
 
 splitAtAndRemove :: Char -> String -> [String]
 splitAtAndRemove c [] = []
@@ -20,32 +26,31 @@ trim :: String -> String
 trim = f . f 
     where f = reverse . dropWhile (==' ')
 
-parseInputLine :: String -> (Int, [Int], [Int])
+parseInputLine :: String -> Game
 parseInputLine s =
     let
         g = splitAtAndRemove ':' s
-        gn =  (head . tail) (splitAtAndRemove ' ' (head g))
+        filteredGame = filter (/="") g
+        gnUnfiltered =  (splitAtAndRemove ' ' (head filteredGame))
+        gn = trace ("uifiltered:" ++ show gnUnfiltered) head.tail $ filter (/="") gnUnfiltered
         nums = splitAtAndRemove '|' ((head . tail)  g)
         win = filter (/="") $ splitAtAndRemove ' ' $ trim (head nums)
         my = filter (/="") $ splitAtAndRemove ' ' $ trim ((head. tail) nums)
-        gameNum = read gn :: Int
+        gameNum = trace (show gn) read gn :: Int
         wins :: [Int]
         wins = map read win
         mine :: [Int]
         mine = map read my
     in
         --trace ("gn: " ++ (show gameNum) ++ "\nwin: " ++ (show wins) ++ "\nmy: " ++ (show mine)) 
-            (gameNum, wins, mine)
+            Game { gid = gameNum, winNumbers =  wins, myNumbers = mine }
         
-firstTriple :: (a,b,c) -> a
-firstTriple (a,_,_) = a
-
-numWins :: (Int, [Int], [Int]) -> Int
-numWins (_, _, []) = 0
-numWins (g, w, (h:t)) = 
+numWins :: Game -> Int
+numWins (Game {gid = _, winNumbers = _, myNumbers = []}) = 0
+numWins (Game {gid = g, winNumbers = w, myNumbers = (h:t)}) = 
     if elem h w
-    then 1 + numWins (g,w,t)
-    else numWins (g,w,t)
+    then 1 + numWins Game { gid = g, winNumbers = w, myNumbers = t}
+    else numWins Game {gid = g, winNumbers = w, myNumbers = t}
     
 winsToPoints :: Int -> Int
 winsToPoints w =
@@ -55,6 +60,27 @@ winsToPoints w =
         if w == 1 
         then 1
         else winsToPoints (w-1) * 2
+
+newCards :: [Game] -> [Game]
+newCards [] = []
+newCards gs = 
+    let 
+        wns = numWins $ head gs
+        gi = gid $ head gs
+
+        addGames :: Int -> Int -> [Game]
+        addGames 0 _  = []
+        addGames w gid' =
+            let 
+                gameIdToAdd = w + gid'
+                gameToAdd = head $ filter (\e -> gid e == gameIdToAdd) gs
+            in 
+                gameToAdd : addGames (w -1) gid'
+
+        added = addGames wns gi
+    in
+        head gs : newCards (added ++ tail gs)
+            
 
 runapp :: String -> Int
 runapp s = 
@@ -66,3 +92,13 @@ runapp s =
     in
         trace (show wins ++ "\npoints: " ++ show points) 
             (sum points)
+
+cardwinapp :: String -> Int
+cardwinapp s = 
+    let 
+        ls = lines s
+        parsed = map parseInputLine ls
+        allCards = newCards parsed
+    in
+        --trace (show parsed) 
+            (length allCards)
